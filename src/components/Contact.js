76926e60a71cd324367e8080623c8571f8701396
Tim Reid <tim.in.canada2014@gmail.com>
2023-09-11 16:21:20 -0300

    Added verification to the copied button in Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,40 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
+  // State variable to track if email is copied
+  const [emailCopied, setEmailCopied] = useState(false);
+
+  // Function to handle email copy
+  const handleCopyEmail = () => {
+    navigator.clipboard.writeText("info@bullsheetmetal.ca");
+    setEmailCopied(true); // Set emailCopied to true
+
+    // Reset emailCopied to false after 3 seconds
+    setTimeout(() => {
+      setEmailCopied(false);
+    }, 3000);
+  };
+
   return (
     <div
       id="contact"
       className="h-screen flex flex-col items-center justify-center bg-primary-bg"
     >
-      {/* Container */}
       <div className="p-8 rounded-lg bg-secondary-bg w-3/4 md:w-1/2 lg:w-1/3">
         <h2 className="text-3xl text-center text-primary-text mb-6">
           Contact Us
         </h2>
-
-        {/* Description */}
         <p className="text-secondary-text mb-4">
           We'd love to hear from you. If you can imagine it, we can bring it to
           life. Let's discuss your ideas and transform them into reality. Reach
           out to us via email:
         </p>
-
-        {/* Email */}
         <div className="text-center">
           <a
             href="mailto:info@yourcompany.com"
             className="text-accent-1 text-lg underline"
           >
-            info@yourcompany.com
+            info@bullsheetmetal.ca
           </a>
         </div>
-
-        {/* Copy Email Button (Optional) */}
         <button
-          onClick={() => navigator.clipboard.writeText("info@yourcompany.com")}
+          onClick={handleCopyEmail}
           className="mt-4 bg-accent-2 text-primary-text px-4 py-2 rounded-full mx-auto block"
         >
-          Copy Email
+          {emailCopied ? "Email Copied!" : "Copy Email"}
         </button>
       </div>
     </div>
